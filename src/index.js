ed2e77da1c750cb6c4cd6fff1f71b81218bd8f04
ckona <pagudonguri@gmail.com>
2020-04-23 21:53:43 +0900

    implement check.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <div className="square">
-        {/* TODO */}
-      </div>
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
     );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
