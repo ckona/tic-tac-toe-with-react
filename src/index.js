950bafaeff60286a7e0dfa4378572372f331aa35
ckona <paugfonguri@gmail.com>
2020-04-24 22:54:44 +0900

    Change Square component to functional

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+const Square = props => (
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+  </button>
+);
 
 class Board extends React.Component {
   renderSquare(i) {
